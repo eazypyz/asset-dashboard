02259b2f48318a47fc946862673be49970620c3a
eazypyz <106640897+eazypyz@users.noreply.github.com>
2026-09-10 13:18:51 +0700

    Remove 'unity3d.com' from the domain list

--- crawler/config.js
+++ crawler/config.js
@@ -17,7 +17,7 @@ export const config = {
     .filter(Boolean)
     .concat([
       // ← Add your domains here
-      "unity3d.com"
+      
     ])
     .filter((v, i, a) => a.indexOf(v) === i), // deduplicate
 
